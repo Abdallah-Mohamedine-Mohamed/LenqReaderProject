a93603983f1defcc714f53f7da9d70c9dcb21807
Abdallah-Mohamedine-Mohamed <abdallahmohamedine17@gmail.com>
2025-10-28 04:41:44 +0100

    Updated index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ensurePromiseWithResolvers } from './utils/ensurePromiseWithResolvers';
 
-// Polyfill pour Promise.withResolvers (nécessaire pour pdf.js sur navigateurs mobiles anciens)
-if (!Promise.withResolvers) {
-  Promise.withResolvers = function <T>() {
-    let resolve: (value: T | PromiseLike<T>) => void;
-    let reject: (reason?: any) => void;
-    const promise = new Promise<T>((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-    return { promise, resolve: resolve!, reject: reject! };
-  };
-}
+// Polyfill pour Promise.withResolvers (nécessaire pour pdf.js sur iOS Safari)
+ensurePromiseWithResolvers();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
