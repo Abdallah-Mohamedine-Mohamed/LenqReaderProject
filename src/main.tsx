da16d2b92cf075a964b8248dff34c340ce452aae
Abdallah-Mohamedine-Mohamed <abdallahmohamedine17@gmail.com>
2025-10-28 04:56:18 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ensurePromiseWithResolvers } from './utils/ensurePromiseWithResolvers';
-
-// Polyfill pour Promise.withResolvers (nécessaire pour pdf.js sur iOS Safari)
-ensurePromiseWithResolvers();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
